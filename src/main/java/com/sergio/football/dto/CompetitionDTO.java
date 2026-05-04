585f87031519bb6627ffc9d4dfaec2ed77942398
SERIVAN19 <sbuenoardila@gmail.com>
2026-05-04 13:19:08 -0500

    feat add associationController crud complete

---
+++ src/main/java/com/sergio/football/dto/CompetitionDTO.java
@@ -0,0 +1,17 @@
+package com.sergio.football.dto;
+
+import java.time.LocalDate;
+
+public class CompetitionDTO {
+
+    private Long id;
+
+    private String name;
+
+    private Integer prizeAmount;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+}
