a69ba532b1d02b1ae214812ec6f8a8acdc4015b0
SERIVAN19 <sbuenoardila@gmail.com>
2026-05-12 11:30:35 -0500

    feat: add user entity, enum role and user repository

---
+++ src/main/java/com/sergio/football/entity/Role.java
@@ -0,0 +1,7 @@
+package com.sergio.football.entity;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/sergio/football/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sergio.football.repository;
+
+import com.sergio.football.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
